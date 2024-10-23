cde1fa184b8f241459ac831a1b1566a74ed58a5c
Asim Abdullayev <abdlyv05@gmail.com>
2024-10-23 14:45:09 +0400

    Add files via upload

---
+++ hocam/assets/js/index.js
@@ -0,0 +1,16 @@
+let bal=95
+if(bal>=50){
+    console.log("Kecdi");
+    if(bal>=95 && bal<=100){
+        console.log("HighHonor");   }
+        else if(bal<=94 && bal>=85){
+            console.log("Honor");
+        }
+        
+        else{console.log("adi Diplom");}
+
+
+    
+
+}
+else{console.log("Kesildi");}
